211cf12438990dad237ab1a208e27f4acfe486a1
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-05-11 18:49:09 +0200

    Caso test de pedido Nacional pasado con éxito

--- mordor-command/src/main/java/pedidos/PedidoNacional.java
+++ mordor-command/src/main/java/pedidos/PedidoNacional.java
@@ -1,20 +1,30 @@
 package pedidos;
 
+import java.util.UUID;
+
 public class PedidoNacional implements Pedido{
 
     private final String id;
     private String destino = null;
     private int peso = 0;
 
-    public PedidoNacional
+    public PedidoNacional(String destino, int peso){
+        this.id = UUID.randomUUID().toString();
+        this.destino = destino;
+        this.peso = peso;
+    }
 
     @Override
     public int peso() {
-        return 0;
+        return this.peso;
     }
 
     @Override
     public String destino() {
-        return null;
+        return this.destino;
+    }
+
+    public String getId(){
+        return this.id;
     }
 }
