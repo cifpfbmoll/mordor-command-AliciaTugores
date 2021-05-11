b898fec4690c63e6eeda020dabb87e32f4fd1df6
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-05-11 19:56:07 +0200

    Añadido y furulando correctamente el método main

--- mordor-command/src/main/java/pingpong/App.java
+++ mordor-command/src/main/java/pingpong/App.java
@@ -1,13 +1,41 @@
 package pingpong;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import pedidos.Pedido;
+import pedidos.PedidoInternacional;
+import pedidos.PedidoPeligroso;
+import pedidos.PedidoPeligrosoOrden;
+import procesador.Oficina;
+import tratramientos.TratamientoPedido;
+import tratramientos.TratamientoPedidoInternacional;
+import tratramientos.TratamientoPedidoPeligroso;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Oficina oficina = new Oficina();
+
+        Pedido pedido = new PedidoInternacional("Comarca", 10);
+        TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
+                (PedidoInternacional) pedido);
+
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+
+        pedido = new PedidoInternacional("Mordor", 10);
+        tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
+
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+
+        pedido = new PedidoPeligrosoOrden("Cima de los vientos",
+                "No urgarse en las uñas con este puñal");
+        TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+
+        pedido = new PedidoPeligrosoOrden("Monte del destino",
+                "No ponerselo en el dedo");
+        peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
+
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
     }
 }
